120b4f996df7d63cc0c447c1bb912d4f6c35a2c2
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2024-01-25 14:47:33 +0500

    Update Q01.cpp

--- Lab01/Q01.cpp
+++ Lab01/Q01.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+    int num;
+    int count;
+    cout << "Enter a Number"<< endl;
+    cin >> num;
+    count = 0;
+    for (int i = 2; i*i <= num ; i++)
+    {
+        if( num%i == 0 ) count++;
+    }
+    if (count>2) cout << "not prime";
+    else cout << "prime";
+}
