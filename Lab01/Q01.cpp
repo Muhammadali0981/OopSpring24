171c902fe6c224fed9e93b133e04ccc53307c6db
Muhammadali0981 <142867504+Muhammadali0981@users.noreply.github.com>
2024-01-29 12:26:18 +0500

    Update Q01.cpp

--- Lab01/Q01.cpp
+++ Lab01/Q01.cpp
@@ -1,3 +1,8 @@
+/* Programmer:Muhammad Ali 23K-0052
+* Date: 25/01/24
+* Descrption:checking if inputted number is prime or not
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -10,8 +15,8 @@ int main(){
     count = 0;
     for (int i = 2; i*i <= num ; i++)
     {
-        if( num%i == 0 ) count++;
+        if(num%i == 0) count++;
     }
-    if (count>2) cout << "not prime";
+    if (count > 2) cout << "not prime";
     else cout << "prime";
 }
